71e2757707a5cb07035dd407a394949673e0805d
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-05-19 22:39:56 +0800

    update

--- src/services/document-notices.service.ts
+++ src/services/document-notices.service.ts
@@ -11,4 +11,18 @@ export const DocumentNoticesService = {
         toast.error("Failed to get document notices list");
       });
   },
+
+  createDocumentNotice: async function (data: any) {
+    return await http
+      .get("/document-notices", data)
+      .then((response) => {
+        toast.success("Successfully create document notice");
+
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Failed to create document notice");
+      });
+  },
 };
