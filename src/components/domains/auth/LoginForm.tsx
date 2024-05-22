67d795b4d9ecb79baa3d67640d8c84ecc257721d
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-05-22 09:21:55 +0800

    update

--- src/components/domains/auth/LoginForm.tsx
+++ src/components/domains/auth/LoginForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 import { Spinner } from "flowbite-react";
 import { AuthService } from "@/services";
 import { Credentials } from "@/types/auth";
@@ -13,6 +14,7 @@ export const LoginForm: React.FC = () => {
     },
   });
 
+  const [showPassword, setShowPassword] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState<boolean>(false);
 
   const handleFormSubmit = handleSubmit(async (credentials: Credentials) => {
@@ -22,6 +24,10 @@ export const LoginForm: React.FC = () => {
       reset();
     });
   });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
   return (
     <div className="w-full">
@@ -35,7 +41,15 @@ export const LoginForm: React.FC = () => {
       ) : (
         <form className="flex flex-col gap-3 text-[14px] mt-2" onSubmit={handleFormSubmit}>
           <input type="text" placeholder="Department Username" {...register("username")} required />
-          <input type="password" placeholder="Password" {...register("password")} required />
+
+          <div className="relative">
+            <input type={showPassword ? "text" : "password"} placeholder="Password" {...register("password")} required />
+            <div className="absolute right-4 top-4">
+              <button type="button" onClick={togglePasswordVisibility}>
+                {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+              </button>
+            </div>
+          </div>
           <button type="submit" className="h-[50px] w-full  text-white bg-primary  rounded mt-2">
             LOG IN
           </button>
